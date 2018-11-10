c2b5832d60ee2f494d80eb48cbc1639487688eb3
Jiyu <jiyu.nam@mail.utoronto.ca>
2018-11-10 18:57:13 -0500

    Data Collection code completed in main.py

--- main.py
+++ main.py
@@ -13,10 +13,20 @@ Pre processing the data:
 
 '''
 
+import os
+import requests
+from bs4 import BeautifulSoup
 
 
-'''
+# =================================== DATA COLLECTION =========================================== #
+page_url = 'https://bushgrafts.com/midi/'
+r = requests.get(page_url)
+data = r.text
+soup = BeautifulSoup(data)
 
-Create a discriminator CNN that can distinguish between authentic and non-authentic jazz music
-
-'''
+i = 1
+for link in soup.findAll('a', href=True):
+    if os.path.splitext(os.path.basename(link['href']))[1] == '.mid':
+        f = requests.get(link['href'])
+        open('./training_data./%s.midi'%(i), 'wb').write(f.content)
+        i = i+1
