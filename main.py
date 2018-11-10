166d49ab76bb03e3ca6ef221f34ae0d44a496dab
Jiyu <jiyu.nam@mail.utoronto.ca>
2018-11-10 16:18:43 -0500

    Description

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 #this is the main file 
 # edited from Jiyu's computer
+
+'''
+
+Pre processing the data:
+    - Download .MIDI files of authentic jazz music, courtesy of https://bushgrafts.com/midi/
+    - Convert format to music21 object, 'stream'
+    - Label this as 'authentic'
+    - Create .MIDI files of non-authentic 'noise' music
+    - Convert format to music21 object, 'stream'
+    - Label this as 'non-authentic'
+
+'''
+
+
+
+'''
+
+Create a discriminator CNN that can distinguish between authentic and non-authentic jazz music
+
+'''
