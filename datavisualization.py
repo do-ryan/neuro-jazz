e66d482f6ef8b07c424bc688991f7a61e99252ae
Ryan Do <ryan.do@mail.utoronto.ca>
2018-11-10 17:53:33 -0500

    working data visualization
    plots pitches vs. time for each instrument

--- datavisualization.py
+++ datavisualization.py
@@ -5,8 +5,82 @@
 '''
 
 from music21 import *
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
+def read_midi(filepath):
+
+        return converter.parse(filepath)
+
+def list_instruments(streamobj):
+        partStream = streamobj.parts.stream()
+        print("List of instruments found on MIDI file:")
+        for p in partStream:
+                print(p.partName)
+
+
+def extract_notes(midi_part):
+        parent_element = []
+        ret = []
+        for nt in midi_part.flat.notes:
+                if isinstance(nt, note.Note):
+                        ret.append(max(0.0, nt.pitch.ps))
+                        parent_element.append(nt)
+                elif isinstance(nt, chord.Chord):
+                        for pitch in nt.pitches:
+                                ret.append(max(0.0, pitch.ps))
+                                parent_element.append(nt)
+
+        return ret, parent_element
+
+
+def print_parts_countour(midi):
+        fig = plt.figure(figsize=(12, 5))
+        ax = fig.add_subplot(1, 1, 1)
+        minPitch = pitch.Pitch('C10').ps
+        maxPitch = 0
+        xMax = 0
+
+        # Drawing notes.
+        for i in range(len(midi.parts)):
+                top = midi.parts[i].flat.notes
+                y, parent_element = extract_notes(top)
+                if (len(y) < 1): continue
+
+                x = [n.offset for n in parent_element]
+                ax.scatter(x, y, alpha=0.6, s=7)
+
+                aux = min(y)
+                if (aux < minPitch): minPitch = aux
+
+                aux = max(y)
+                if (aux > maxPitch): maxPitch = aux
+
+                aux = max(x)
+                if (aux > xMax): xMax = aux
+
+        for i in range(1, 10):
+                linePitch = pitch.Pitch('C{0}'.format(i)).ps
+                if (linePitch > minPitch and linePitch < maxPitch):
+                        ax.add_line(mlines.Line2D([0, xMax], [linePitch, linePitch], color='red', alpha=0.1))
+
+        plt.ylabel("Note index (each octave has 12 notes)")
+        plt.xlabel("Number of quarter notes (beats)")
+        plt.title('Voices motion approximation, each color is a different instrument, red lines show each octave')
+        plt.show()
+
+
+        # Focusing only on 6 first measures to make it easier to understand.
+        print_parts_countour(base_midi.measures(0, 6))
 
 def main():
+
+        example1 = read_midi("training_data/thejazzpage/chega.midi")
+        print_parts_countour(example1)
+
+        list_instruments(example1)
+
+        return
 
 if __name__ == "__main__":
         main()
