18efc0aca15f2689877f71e8ae3816e0011fff02
Jiyu <jiyu.nam@mail.utoronto.ca>
2018-11-12 16:07:03 -0500

    edited instrument list and main file

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # Main file
 
+from datavisualization import list_instruments
+from music21 import *
+import os
+
+instruments = []
+directory = 'training_data/authentic'
+for file in os.listdir(directory):
+    data = converter.parse(os.path.join(directory, file))
+    instruments.append(list_instruments(data))
+
+print(3)
+if __name__ == "__main__":
+    main()
