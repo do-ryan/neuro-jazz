111e5da2a8bf6fd46a1357c1c2ee02fc7e56f2b9
Jiyu <jiyu.nam@mail.utoronto.ca>
2018-11-12 23:21:44 -0500

    moved authentic piano to datacollection

--- datacollection.py
+++ datacollection.py
@@ -11,13 +11,12 @@ r = requests.get(page_url)
 data = r.text
 soup = BeautifulSoup(data)
 
-# Authentic music
 i = 1
-for link in soup.findAll('a', href=True):
-    if os.path.splitext(os.path.basename(link['href']))[1] == '.mid':
-        f = requests.get(link['href'])
-        open('./training_data./authentic./%s.midi'%(i), 'wb').write(f.content)
-        i = i+1
+# for link in soup.findAll('a', href=True):
+#     if os.path.splitext(os.path.basename(link['href']))[1] == '.mid':
+#         f = requests.get(link['href'])
+#         open('./training_data./authentic./%s.midi'%(i), 'wb').write(f.content)
+#         i = i+1
 
 
 # Non-authentic music
@@ -29,39 +28,40 @@ fields = ['tonality_factor', 'set_mode', 'tonic_endpoints', 'proximity_factor', 
 values = ['0', '-1', '0', '0', '1', '0', '1', '0', '200']
 
 # use selenium to open the page, locate the necessary fields, clear the input, enter new input, and then click submit
-download_dir = r"C:\Users\jiyun\PycharmProjects\mie324\NeuroJazz\training_data\nonauthentic"
-options = webdriver.ChromeOptions()
-options.add_experimental_option('prefs', {"download.default_directory": download_dir})
-driver = webdriver.Chrome(r'C:\Users\jiyun\Downloads\chromedriver_win32/chromedriver.exe', chrome_options=options)
+# download_dir = r"C:\Users\jiyun\PycharmProjects\mie324\NeuroJazz\training_data\nonauthentic"
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option('prefs', {"download.default_directory": download_dir})
+# driver = webdriver.Chrome(r'C:\Users\jiyun\Downloads\chromedriver_win32/chromedriver.exe', chrome_options=options)
 
-for j in range(300):
-    i = 0
-    driver.get(page_url)
-    for i in range(len(fields)):
-        sbox = driver.find_element_by_name(fields[i])
-        sbox.clear()
-        sbox.send_keys(values[i])
-        i = i+1
-    sbox.submit()
-    link = driver.find_elements_by_link_text('MIDI file')[0]
-    link.click()
-    j = j+1
+# for j in range(300):
+#     i = 0
+#     driver.get(page_url)
+#     for i in range(len(fields)):
+#         sbox = driver.find_element_by_name(fields[i])
+#         sbox.clear()
+#         sbox.send_keys(values[i])
+#         i = i+1
+#     sbox.submit()
+#     link = driver.find_elements_by_link_text('MIDI file')[0]
+#     link.click()
+#     j = j+1
 
 
 # Clean authentic data to only be piano music
 directory = 'training_data/authentic'
 for file in os.listdir(directory):
     data = converter.parse(os.path.join(directory, file))
-    s2 = instrument.partitionByInstrument(data)
-    temp = stream.Stream()
-    if s2 is not None:
-        if len(s2) == 1:
-            data.write('midi', './training_data./authenticpiano./%s' % (file))
-        else:
-            for part in s2:
-                if isinstance(part.getInstrument(), instrument.Piano) or part == None:
-                    temp.append(part)
-            temp.write('midi', './training_data./authenticpiano./%s' % (file))
-    elif s2 is None:
-        data.write('midi', './training_data./authenticpiano./%s' % (file))
+    # s2 = instrument.partitionByInstrument(data)
+    # temp = stream.Stream()
+    # if s2 is not None:
+    #     if len(s2) == 1:
+    #         data.write('midi', './training_data./authenticpiano./%s' % (file))
+    #     else:
+    #         for part in s2:
+    #             if isinstance(part.getInstrument(), instrument.Piano):
+    #                 temp.append(part)
+    #         temp.write('midi', './training_data./authenticpiano./%s' % (file))
+    # elif s2 is None:
+    #     data.write('midi', './training_data./authenticpiano./%s' % (file))
+    data.write('midi', './training_data./authenticpiano./%s' % (file))
     print('%s'%(file))
--- models.py
+++ models.py
@@ -6,23 +6,18 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2dd().double()
-        self.pool = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d().double()
-        self.conv3 = nn.Conv2d().double()
-        self.fc1 = nn.Linear()
-        self.fc2 = nn.Linear()
-        self.fc3 = nn.Linear()
-        self.bn1 = nn.BatchNorm2d().double()
-        self.bn2 = nn.BatchNorm2d().double()
-        self.bn3 = nn.BatchNorm2d().double()
+        self.conv1 = nn.Conv2d(301, 10, 10).double()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(10, 20, 5).double()
+        self.fc1 = nn.Linear(20 * 29 * , 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
 
 
     def forward(self, x):
-        x = self.pool(self.bn1(F.relu(self.conv1(x))))
-        x = self.pool(self.bn2(F.relu(self.conv2(x))))
-        x = self.pool(self.bn3(F.relu(self.conv3(x))))
-        x = x.view()
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 20 * 29 *  )
         x = F.relu(self.fc1(x.float()))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
