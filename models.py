fa4cdbfe5e1631769d02eff720710f6437a3619f
Ryan Do <ryan.do@mail.utoronto.ca>
2018-11-26 21:57:36 -0500

    decent results

--- models.py
+++ models.py
@@ -11,16 +11,16 @@ class CNN(nn.Module):
 
         #p = 0 padding
         s = 1
-        k1 = (12, 96)
+        k1 = (4, 4)
         k2 = (2, 2)
         #L = (133,26502)
         num_output_featuremaps = 5
 
         #self.fc1 = nn.Linear(133*46842, 1).double()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(k1[0],k1[1]), stride=(12, 48)).double()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(k1[0],k1[1]), stride=k1).double()
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=num_output_featuremaps, kernel_size=(k2[0],k2[1]), stride=k2).double()
         #self.fc_inputsize = int((((L[0]-k1[0])/s+1-k2[0])/s+1)*(((L[1]-k1[1])/s+1-k2[1])/s+1)*num_output_featuremaps)
-        self.fc_inputsize = int(280/batch_size) # size of fully connected input
+        self.fc_inputsize = int(11520/batch_size) # size of fully connected input
         self.pool = nn.MaxPool2d(2,2)
         self.fc1 = nn.Linear(self.fc_inputsize, 2000).double()
         self.fc2 = nn.Linear(2000, 500).double()
@@ -28,13 +28,12 @@ class CNN(nn.Module):
 
     def forward(self, x):
         x = torch.unsqueeze(x, dim=1)
-        x = x.permute(0, 1, 2, 3)
         x = F.relu(self.conv1(x))
         x = self.pool(x)
         x = F.relu(self.conv2(x))
         x = x.squeeze()
         x = x.view(-1, self.fc_inputsize)
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
         x = F.relu(self.fc2(x))
         x = torch.sigmoid(self.fc3(x))
         x = x.squeeze(1)
